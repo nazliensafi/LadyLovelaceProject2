f51a3694c898c697e19b4919d1e8bc360628fa62
laurenwuu <laurenwu01@gmail.com>
2022-11-26 23:39:13 -0500

    stub code a star

--- search.py
+++ search.py
@@ -183,3 +183,82 @@ def gbfs_h1(board):
     # "Solution path : " #I'm not sure how to keep track of the actual moves made for the path :/
     # final Board object displayed as 2D matrix
 
+
+# A Star algorithm: calclate for heuristic and cost 
+def astar(board):
+    initial_state = board
+    open = []
+    closed = []
+
+    #index in the closed set to keep track of the path
+    parentIndex = 0
+
+    #to calculate the runtime
+    start = time.time()
+
+    #each node passed in the queues = a tuple where the elements are: (board, parent state's index, cost)
+    #current configuration: visited = [(S, 0, 0)], open = [], closed = () 
+    # S = initial game state
+    visited = (initial_state, parentIndex, board.h1())
+
+
+    while(visited != []):
+        # find a list of next possible moves from the current state
+        # #replace [] with a call to the function that checks all possible moves from the state in the visited queue and return a new Board
+        # ex. nextMove = possibleMove(visited[0])
+        nextMove = [] 
+   
+        #if there is no next move and we did not reach the goal, there is no solution
+        if(nextMove == []):
+            stop = time.time()
+            print("No solution")
+            break #stop while loop
+
+        if (nextMove != []):
+            
+            parentIndex+=1
+
+            for m in nextMove:
+                #if next move is found and we reach the goal, we finished the search
+                if(m is goal()):
+                   goalstate = (m, parentIndex, 0)
+                   visited.pop()
+                   stop = time.time()
+                   break
+
+                #else, we do the following steps:
+                else:
+                    #for each of the possible move we found (for each element of the array nextMove)
+                    #calculate the value of h1()
+                    hn = m.h1()
+                    #find the right index where to insert the new m
+                    i = 0
+                    for n in open:
+                        if(n[2] < hn):
+                            i+=1
+                        else:
+                            open.insert(i, m)
+            #after verifying each child node        
+            #empty nextMove
+            nextMove.clear()
+            #append the element from visited queue to closed queue
+            closed.append(visited)
+            #append next state in the 'open' queue to visited and delete the same element from the open queue
+            visited = open[0]
+            open.pop(0)
+
+
+    #as result, we should display:
+    runtime = stop-start
+
+    #find the actual path by tracking the parent node
+    path = [goalstate]
+    currentNode = goalstate
+    while(currentNode != initial_state):
+        #get the state at the index of the parent node of the current node
+        i = currentNode[1]
+        path.insert(0, closed[i])
+        currentNode = closed[i]
+    
+    #lastly, we insert the initial_state in the beginning of the path
+    path.insert(0, initial_state)
